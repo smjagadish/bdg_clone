d423d65dc48f12714b9691709d156b9e475c88fe
S M Jagadish <s.m.jagadish@ericsson.com>
2023-05-25 16:16:12 -0400

    updated sample impl for MDC in server interceptor- to be enhanced further

--- grpc/src/main/java/com/baeldung/grpc/sample/sampleServerInterceptor.java
+++ grpc/src/main/java/com/baeldung/grpc/sample/sampleServerInterceptor.java
@@ -6,13 +6,16 @@ import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class sampleServerInterceptor implements ServerInterceptor {
     private static final Logger LOGGER = LoggerFactory.getLogger(sampleServerInterceptor.class);
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        MDC.put("transaction.id","CXD456");
         LOGGER.info("inside server intercepto#1");
         System.out.println("first server interceptor and no modifications");
+        MDC.clear();
         return next.startCall(call,headers);
     }
 
